1e29c1cbc690534f5846a643e413ceb521220a97
abdullahiahmed <aahmed_02@hotmail.com>
2021-12-02 21:56:12 +0000

    lotide

--- head.js
+++ head.js
@@ -14,3 +14,6 @@ const assertEqual = function(actual, expected) {
   
   assertEqual(head([5,6,7]),5);
   assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+  module.exports = {head}
+  
